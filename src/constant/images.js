42ea983ac69104c4d7abcf4ebcfed474ed6b5caf
hossein <hosseinbakhtiari781@outlook.com>
2025-01-13 22:44:03 +0330

    CTA cart part 1

--- src/constant/images.js
+++ src/constant/images.js
@@ -2,6 +2,8 @@ import Logo from '../assets/Logo.svg';
 import HeroImage from "../assets/HeroImage.svg"
 import PostImage from "../assets/posts/post1.jpg"
 import PostProfileImage from "../assets/posts/post-profile.svg"
+
+
 const images = {
     Logo,
     HeroImage,
