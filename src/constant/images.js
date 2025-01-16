c5efeab9eaa61be0b5437dde22b5675b0122cd79
hossein <hosseinbakhtiari781@outlook.com>
2025-01-16 10:42:33 +0330

    finish CTA section

--- src/constant/images.js
+++ src/constant/images.js
@@ -2,13 +2,14 @@ import Logo from '../assets/Logo.svg';
 import HeroImage from "../assets/HeroImage.svg"
 import PostImage from "../assets/posts/post1.jpg"
 import PostProfileImage from "../assets/posts/post-profile.svg"
-
+import CTAimage from "../assets/CtaImage.jpg"
 
 const images = {
     Logo,
     HeroImage,
     PostImage,
     PostProfileImage,
+    CTAimage
 }
 
 export default images
