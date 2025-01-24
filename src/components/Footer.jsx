fd94812f653ded3ffe83dcf57ce7ae81faec3b29
hossein <hosseinbakhtiari781@outlook.com>
2025-01-24 10:53:52 +0330

    responsive footer for Tablet devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import images from "../constant/images";
 import { FaFacebook } from "react-icons/fa";
-import { AiFillInstagram, AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 import { BsTelegram } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5">
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold ">Product</h3>
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12">
+        <div className="col-span-5 md:col-span-4">
+          <h3 className="text-dark-light font-bold md:text-lg ">Product</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
             <li>
               <a href="/">Landingpage</a>
@@ -28,8 +33,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold ">Services</h3>
+        <div className="col-span-5 md:col-span-4">
+          <h3 className="text-dark-light font-bold md:text-lg ">Services</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
             <li>
               <a href="/">Documentation</a>
@@ -48,8 +53,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold ">Company</h3>
+        <div className="col-span-5 md:col-span-4 md:col-start-5">
+          <h3 className="text-dark-light font-bold md:text-lg ">Company</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
             <li>
               <a href="/">About</a>
@@ -65,8 +70,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="col-span-5">
-          <h3 className="text-dark-light font-bold ">More</h3>
+        <div className="col-span-5 md:col-span-4">
+          <h3 className="text-dark-light font-bold md:text-lg ">More</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4">
             <li>
               <a href="/">Documentation</a>
@@ -80,36 +85,52 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="col-span-10 ">
-          <img src={images.Logo} alt="logo" className="brightness-0 invert mx-auto" />
-          <p className="text-sm text-dark-light text-center mt-4">Build a modern and creative website with crealand</p>
-          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300">
-          <li>
+        <div className="col-span-10 md:order-first md:col-span-4">
+          <img
+            src={images.Logo}
+            alt="logo"
+            className="brightness-0 invert mx-auto md:mx-0"
+          />
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base">
+            Build a modern and creative website with crealand
+          </p>
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
+            <li>
               <a href="/">
-                <AiOutlineTwitter classname="w-6 h-auto"/>
+                <AiOutlineTwitter classname="w-6 h-auto" />
               </a>
             </li>
             <li>
               <a href="/">
-                <FaFacebook classname="w-6 h-auto"/>
+                <FaFacebook classname="w-6 h-auto" />
               </a>
             </li>
             <li>
               <a href="/">
-                <AiFillYoutube classname="w-6 h-auto"/>
+                <AiFillYoutube classname="w-6 h-auto" />
               </a>
             </li>
             <li>
               <a href="/">
-                <AiFillInstagram classname="w-6 h-auto"/>
+                <AiFillInstagram classname="w-6 h-auto" />
               </a>
             </li>
             <li>
               <a href="/">
-                <BsTelegram classname="w-6 h-auto"/>
+                <BsTelegram classname="w-6 h-auto" />
               </a>
             </li>
           </ul>
+        </div>
+
+        <div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12">
+          <div className="bg-primary text-white p-3 rounded-full">
+            <AiFillHeart className="w-7 h-auto" />
+          </div>
+          <p className="font-bold italic text-dark-light ">
+            {" "}
+            Copyright © 2023. Crafted with love.
+          </p>
         </div>
       </footer>
     </section>
